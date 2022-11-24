9283ee6397c11091783bd86efdc37c28a4973dbb
Pedro Felix <pereraflix15@gmail.com>
2022-11-24 00:17:14 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,16 @@ import styles from './Header.module.css'
 
 import Subtitle from "../subtitle/Subtitle"
 import Title from "../title/Title"
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Pedro</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName='github' link='https://github.com/Gilmelandia'/>
+      </div>
     </div>
   )
 }
