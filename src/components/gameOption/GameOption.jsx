a3da67e6a7ddb0d49e4700dc061e40dca96bfe0a
Pedro Felix <pereraflix15@gmail.com>
2022-11-27 12:03:58 -0300

    criado estados no gameOption

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,8 +1,15 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption () {
+function GameOption ({ status }) {
   return (
     <div className={styles.gameOption}>
+      {
+        status === 1 && <Icon iconName='circle' size='25px'/>
+      }
+      {
+        status === 1 && <Icon iconName='X' size='25px'/>
+      }
     </div>
   )
 }
